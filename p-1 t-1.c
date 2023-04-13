8f80324bffbf50bd2b4449de97429728aa1db8c1
Dixitkumbhani31 <130464783+Dixitkumbhani31@users.noreply.github.com>
2023-04-13 21:12:02 +0530

    Update p-1 t-1.c

--- p-1 t-1.c
+++ p-1 t-1.c
@@ -8,10 +8,15 @@ main()
 	printf("* \t * \n");
 	printf("    * \t     * \n");
 	printf("\t * \t * \n");
-	printf("\n");
-	printf("\n");
-	printf("\n");
+	printf("\n\n\n\n\n\n");
 	
-	printf(" * \t * \t * \t * \t * \t * \t *\n");
+	printf(" \t*\t*\t*\t*\t*\t*\t*\n\n");
+	printf("\n *\t*\t*\t*\t*\t*\t*\t*\n");
+	printf(" *           \t*\t\t\t                *\n");
+	printf(" *           \t*\t \t*\t*\t*\t*\n");
+	printf(" *           \t*\t \t*\t        *\t*\n");
+	printf(" *\t*\t*\t*\t*\t*\t*\t*");
+	
+	
 	
 }
